f90e0ec2a3d57a18651c5d11772f88d6fb87e6cb
nianfanchao <2580374475@qq.com>
2018-06-16 11:35:28 +0800

    Initial Commit

---
+++ MagicAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicAnimation
+//
+//  Created by 九尾 on 2018/6/16.
+//  Copyright © 2018年 九尾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
